6f3175e6685d4169d39c965987a0c258b0a55003
vitormaduro <vitormaduro006@gmail.com>
2023-03-16 08:56:52 -0300

    Semi-final implementation of Gallery. Missing assets

--- Scripts/Gallery.cs
+++ Scripts/Gallery.cs
@@ -6,5 +6,12 @@ public partial class Gallery : Control
 	public override void _Ready()
 	{
 		GetNode<TextureButton>("BackButton").Pressed += QueueFree;
+		GetNode<TextureButton>("%KaedeButton").Pressed += DisplayKaede;
+	}
+
+	public void DisplayKaede()
+	{
+		GetNode<Sprite2D>("Left").Texture = GD.Load<Texture2D>("res://Art/Sprites/Kaede/kaede_neutral.png");
+		GetNode<Sprite2D>("Right").Texture = GD.Load<Texture2D>("res://Art/Sprites/Kaede/kaede_smile.png");
 	}
 }
